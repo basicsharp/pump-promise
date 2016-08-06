e6f3c187decaf34ade0ce2ed374146aa558ed093
Porramate Lim <basicsharp@gmail.com>
2016-08-07 01:54:41 +0700

    Bump version to v1.0.0
    - Remove unnecessary code

--- index.js
+++ index.js
@@ -2,15 +2,8 @@
 
 var pump = require('pump');
 
-var noop = function () {}
-
-var isFn = function (fn) {
-  return typeof fn === 'function'
-}
-
 var pumpPromise = function() {
   var streams = Array.prototype.slice.call(arguments);
-  isFn(streams[streams.length - 1] || noop) && streams.pop() || noop;
   if (Array.isArray(streams[0])) streams = streams[0];
   if (streams.length < 2) throw new Error('pump requires two streams per minimum');
 
